55f4eabdf5264112b89b598122c23e1aa57e22f7
ElijahKlein <klein93@purdue.edu>
2023-01-29 18:27:30 -0500

    Update to current directory/file system

---
+++ Net Score Calculation/Metric Calculation/RampUp.rs
@@ -0,0 +1,8 @@
+/*  Name: 
+ *  Date of Last Edit:
+ *  
+ *  Purpose: Calculate Ramp Up time Sub Metric of a given Github Repository
+ *
+ *  Details: 
+*/
+
---
+++ Net Score Calculation/Metric Calculation/Responsiveness.rs
@@ -0,0 +1,7 @@
+/*  Name:
+ *  Date of Last Edit:
+ *  
+ *  Purpose: Calculate Responsiveness Metric of a given Github Repository
+ *
+ *  Details: Language not set, revamp file in new language if needed
+*/
