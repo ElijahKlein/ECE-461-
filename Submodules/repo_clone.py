4de6d0ebb0a9f969641dfe0b0e545e958df478b3
Matthew-Nale <mnale@purdue.edu>
2023-02-08 17:22:46 -0500

    Added Pull Request API usage, and examples in main.py

--- Submodules/repo_clone.py
+++ Submodules/repo_clone.py
@@ -12,7 +12,10 @@ import os
 
 def clone_repo(url):
     try:
-        clonedRepo = git.Repo.clone_from(url, 'C:/temp/' + os.path.basename(url))       #Clones the repo from the provided URL into 'temp' and returns the Repo (GitPython) structure
-        return clonedRepo
+        return git.Repo.clone_from(url, 'C:/temp/' + os.path.basename(url))             #Clones the repo from the provided URL into 'temp' and returns the Repo (GitPython) structure
     except:
-        print("Error, provide new valid repository URL")                                #Cloning failed, provides an error code
+        if(os.path.exists('C:/temp/' + os.path.basename(url))):
+            try:
+                return git.Repo('C:/temp/' + os.path.basename(url))
+            except: 
+                print("Error, provide new valid repository URL")                        #Cloning failed, provides an error code
