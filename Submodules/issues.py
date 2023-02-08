83bc8a452b1caf7de6a691c34e2e724b59e10b3b
Eric Chen <eicchen02@gmail.com>
2023-02-08 17:32:18 -0500

    Moved clone location and made token file

--- Submodules/issues.py
+++ Submodules/issues.py
@@ -8,17 +8,16 @@ Name: Matthew Nale
 
 """
 
-from github import Github                                      #Using PyGitHub for interaction with REST API
+
 import os
 import sys
-
+from Submodules.token_file import *
 
 #getIssuesByType can be slow depending on the amount of issues. Look into another way of doing this. Type can be either 'closed', 'open', or 'all'
 def getIssuesByType(url, type):
     count = 0                                                  #Count used to count the type of issues being found
-    g = Github('PRIVATE ACCESS TOKEN HERE')                    #TODO MY ACCESS TOKEN, DO NOT SHARE. PUT YOUR OWN TOKEN HERE FOR NOW
     try:
-        repo = g.get_repo(url.split("github.com/", 1)[1])      #Obtains the name identifier from the provided url, and obtains that repo from the REST API
+        repo = token.get_repo(url.split("github.com/", 1)[1])      #Obtains the name identifier from the provided url, and obtains that repo from the REST API
         issuesAndPull = repo.get_issues(state=type)            #Get the issues and pull requests depending on the provided state
         for issue in issuesAndPull:
             if not issue.pull_request:                         #Sort out the Pull Requests from the issues. For some reason PyGitHub combines the two
---
+++ Submodules/token_file.py
@@ -0,0 +1,3 @@
+from github import Github                                      #Using PyGitHub for interaction with REST API
+
+token = Github('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')                    #! MY ACCESS TOKEN, DO NOT SHARE. PUT YOUR OWN TOKEN HERE FOR NOW
