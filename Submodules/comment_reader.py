8b49e283779a30dee22173984d1032ae81c6f35b
Matthew-Nale <mnale@purdue.edu>
2023-02-11 16:49:43 -0500

    Changed filename to be more accurate

--- Submodules/comment_reader.py
+++
@@ -1,9 +0,0 @@
-""" 
-Name: Eric Chen 
-    Date of Last Edit: 2/8/2023
-    
-    Purpose: Reads and counts comments 
-
-    Details: Will probably use regex to achieve this
-
-"""
---
+++ Submodules/file_information.py
@@ -0,0 +1,9 @@
+""" 
+Name: Matthew Nale
+    Date of Last Edit: 2/11/2023
+    
+    Purpose: Used for performing analysis on cloned local files from the given repository
+
+    Details: Has multiple functions that can be called to obtain information about all files/specific files
+
+"""
