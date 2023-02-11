d734e92e9c2450583e106487caefd752f22f523f
Matthew-Nale <mnale@purdue.edu>
2023-02-11 16:01:46 -0500

    Integrated Python-Rust functionality

--- NetScoreCalculation/NetScore.rs
+++ NetScoreCalculation/NetScore.rs
@@ -31,11 +31,27 @@ fn main() {
 }*/
 
 //? Uncomment this block to test the RampUp.rs function. Takes 2 argv values (readme size, average comments per file)
-
+/*
 fn main() {
     let args : Vec<String> = env::args().collect();                                     //Collects the argv values into a vector called args
     let readme_size : f64 = args[1].parse().unwrap();                                   //Converst the string values into a i32 value 
     let num_comments : f64 = args[2].parse().unwrap();
     let readme_weight = RampUp::calculate_readme(readme_size);
     println!("README size weighting: {readme_weight}");
+}*/
+
+//? Used for overall NetScore function
+//* Args for this main are (num open issues, num stars, readme length (lines), num of comments)
+fn main() {
+    let args : Vec<String> = env::args().collect();
+    let open_issues : f64 = args[1].parse().unwrap();
+    let stars : f64 = args[2].parse().unwrap();
+    let readme_size : f64 = args[3].parse().unwrap();
+    let num_comments : f64 = args[4].parse().unwrap();
+
+    let correctness_base : f64 = Correctness::calculate_correctness(open_issues, stars);
+    let readme_weight : f64 = RampUp::calculate_readme(readme_size);
+
+    println!("Base Correctness Weighting is: {correctness_base}");
+    println!("README size weighting is: {readme_weight}");
 }
