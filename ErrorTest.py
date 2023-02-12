8f1f42860a8d55757e793d4ebf190769be317d24
ElijahKlein <klein93@purdue.edu>
2023-02-12 18:48:22 -0500

    Updated to ErrorTest, testing how it effects percentage

--- ErrorTest.py
+++ ErrorTest.py
@@ -30,91 +30,145 @@ failCount = 0
 
 repo = clone_repo(url)
 
+
 total += 1	
 try:
 	assert(repo) != None
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(npm2git("https://www.npmjs.com/package/express")) == "https://github.com/expressjs/express"
 except AssertionError as msg:
 	failCount += 1
-total += 1
-try:
-	assert(getDirectorySize(url)) == 5 #TODO - verify
-except AssertionError as msg:
-	failCount += 1
+	print(total)
+
 total += 1	
 try:
-	assert(getDirectorySize(url)) == 19
+	assert(getDirectorySize(url)) == 19 #TODO - Verify
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(issues.getIssuesByType(url, 'open')) == 6
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(issues.getIssuesByType(url, 'closed')) == 4
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(issues.getUsers(url)) == 1
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(pulls.getMostRecentPull(url, 'closed')) == (datetime.now() - datetime(2023, 2, 11, 20, 42)).days
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(pulls.getMostRecentPull(url, 'open')) == None #TODO - verify
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(pulls.getAllPulls(url, 'closed')) == 2
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
 total += 1	
 try:
 	assert(pulls.getAllPulls(url, 'open')) == 0
 except AssertionError as msg:
 	failCount += 1
+	print(total)
 total += 1	
 try:
 	assert(pulls.getCreationDate(url)) == (datetime.now() - datetime(2023, 2, 11, 19, 4)).days
 except AssertionError as msg:
 	failCount += 1
+	print(total)
 total += 1	
 try:
 	assert(readme.checkLicensing(repo)) == 1 #TODO - verify
 except AssertionError as msg:
 	failCount += 1
+	print(total)
 total += 1	
 try:
 	assert(readme.checkRMLength(repo)) == 707
 except AssertionError as msg:
 	failCount += 1
+	print(total)
+
+
+
+
+
 total += 1	
+numContrib = contrib.getNumContributors(url).totalCount
 try:
-	assert(contrib.getNumContributors(url)) == 2
+	#print(f'NumContrib: {contrib.getNumContributors(url)}')
+	assert(contrib.getNumContributors(url).totalCount) == 2
 except AssertionError as msg:
 	failCount += 1
+	print("fail 1")
 total += 1	
+numFiles = contrib.getNumFiles(url)
 try:
 	assert(contrib.getNumFiles(url)) == 5 #TODO - verify
 except AssertionError as msg:
 	failCount += 1
-print(contrib.getStatsContributors(url))
+	print("fail 2")
+	
+	
+#TODO - This Fails
+total += 1	
+try:
+	#print(f'ContribStats: {contrib.getStatsContributors(url)}')
+	assert(contrib.getStatsContributors(url)) == 5 #TODO - verify
+except AssertionError as msg:
+	failCount += 1
+	print("fail 3")
+	
+#TODO - This Fails
+total += 1	
+try:
+	#print(f'commitactivity: {contrib.getStatsCommitActivity(url)}')
+	assert(contrib.getStatsCommitActivity(url)) == 5 #TODO - verify
+except AssertionError as msg:
+	failCount += 1
+	print("fail 4")
+total += 1	
+try:
+	assert(contrib.calcContributorsPerFile(numContrib, numFiles)) == 0.4 #TODO - verify
+except AssertionError as msg:
+	failCount += 1
+	print("fail 5")
+
+
+
 
 
 print(f'{total - failCount}/{total} test cases passed. Z% line coverage achieved.')
-
 
 
 """
