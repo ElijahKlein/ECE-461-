888e346cf1020d917fe8d42ca286bd3c6ed7bb50
ElijahKlein <klein93@purdue.edu>
2023-02-12 16:33:07 -0500

    First update on Errortesting

--- ErrorTest.py
+++ ErrorTest.py
@@ -7,18 +7,77 @@
 """
 
 import sys
+#import pytest
+from datetime import datetime
 import Submodules.global_var as gv
-from NetScoreCalculation.MetricCalculation.Licensing import calculateLicenseScore
 from Submodules.repo_clone import clone_repo
-import Submodules.issues as issues
+from Submodules.file_information import getDirectorySize
+from Submodules.npm_handler import npm2git 
 import Submodules.pull_requests as pulls
-import pytest
 
+from NetScoreCalculation.MetricCalculation.Licensing import calculateLicenseScore
+import Submodules.issues as issues
 
 
 
 url = 'https://github.com/ElijahKlein/461-Test-Case-1'
 repo = clone_repo(url)                                                          #Clones the repository from the given URL, and a GitPython Repo object is stored in repo
+
+total = 0
+failCount = 0
+
+total += 1
+try:
+	assert(getDirectorySize(url)) == 5
+except AssertionError as msg:
+	failCount += 1
+total += 1	
+try:
+	assert(getDirectorySize(url)) == 19
+except AssertionError as msg:
+	failCount += 1
+total += 1	
+try:
+	assert(issues.getIssuesByType(url, 'open')) == 6
+except AssertionError as msg:
+	failCount += 1
+total += 1	
+try:
+	assert(issues.getIssuesByType(url, 'closed')) == 4
+except AssertionError as msg:
+	failCount += 1
+total += 1	
+try:
+	assert(issues.getUsers(url)) == 1
+except AssertionError as msg:
+	failCount += 1
+total += 1	
+try:
+	assert(npm2git("https://www.npmjs.com/package/express")) == "https://github.com/expressjs/express"
+except AssertionError as msg:
+	failCount += 1
+total += 1	
+try:
+	assert(pulls.getMostRecentPull(url, 'closed')) == (datetime.now() - datetime(2023, 2, 11, 20, 42)).days
+except AssertionError as msg:
+	failCount += 1
+
+
+
+print(f'{total - failCount}/{total} test cases passed. Z% line coverage achieved.')
+
+
+
+"""
+
+total += 1	
+try:
+	assert( ) == 
+except AssertionError as msg:
+	failCount += 1
+
+
+
 license_score = calculateLicenseScore(repo)                                     #license_score is determined by the evaluate_readme function in Licensing.py
 print(f'License scoring: {license_score}')
 
@@ -33,3 +92,4 @@ recentPull = pulls.getMostRecentPull(url, 'closed')                             
 print(f'The most recent pull request was: {recentPull} time ago')
 #pullDates = pulls.getAllPullDates(url, 'closed')                               #Example usage of the getAllPullDates function, which obtains a list of all Pull Request dates
 #print(pullDates)
+"""
