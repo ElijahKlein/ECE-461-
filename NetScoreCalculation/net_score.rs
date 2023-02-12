4bfd8e2c7f19f6899c745ecc194efb552791df76
Matthew-Nale <mnale@purdue.edu>
2023-02-11 21:05:36 -0500

    Passing of License score to net_score.rs

--- NetScoreCalculation/net_score.rs
+++ NetScoreCalculation/net_score.rs
@@ -20,13 +20,15 @@ use std::env;
 //*Compile NetScore.rs with the 'rustc NetScore.rs' command, and run the executable with 3 args (open issues, closed issues, users).
 fn main() {
     let args : Vec<String> = env::args().collect();                                     //Collects the argv values into a vector called args
-    let open : f64 = args[1].parse().unwrap();                                          //Converst the string values into a f64 value for all args 
-    let users : f64 = args[2].parse().unwrap();
-    let readme_length : f64 = args[3].parse().unwrap();
+    let license_base : f64 = args[1].parse().unwrap();
+    let open : f64 = args[2].parse().unwrap();                                          //Converst the string values into a f64 value for all args 
+    let users : f64 = args[3].parse().unwrap();
+    let readme_length : f64 = args[4].parse().unwrap();
 
     let correct_base = correctness::calculate_correctness(open, users);                 //Calls the calculate_percentage to find the correctiveness base value
     let ramp_base = ramp_up::calculate_rampup(readme_length);
 
     println!("Base Correctness Weighting is: {correct_base}");
     println!("Base RampUp Weighting is: {ramp_base}");
+    println!("Base License Weighting is: {license_base}");
 }
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ else:
     url = url_in                                                                #Obtains the URL link from argv[1]. Will later be modified to take a .txt file instead
 repo = clone_repo(url)                                                          #Clones the repository from the given URL, and a GitPython Repo object is stored in repo
 license_score = calculateLicenseScore(repo)                                     #license_score is determined by the evaluate_readme function in Licensing.py
-print(f'License scoring: {license_score}')
+
 readmeLength = rm.checkRMLength(repo)                                           #Example usage of the checkRMLength function in readme.py, which returns the lines of the README
 print(f'Number of lines in the README: {readmeLength}')
 repoSize = files.getDirectorySize(url)                                          #Example usage of the getDirectorySize function, which returns the size of the directory
@@ -46,5 +46,6 @@ creationDate = pulls.getCreationDate(url)
 print(f'The creation date of the repository was: {creationDate}')
 
 executable = os.path.dirname(__file__) + "/NetScoreCalculation/net_score.exe "
-args = f"{numIssues} {numDownloads} {readmeLength} 100"                         #Arguements for NetScore file. Add more as needed
+#Arguements for NetScore file. Add more as needed
+args = f"{license_score} {numIssues} {numDownloads} {readmeLength} 100"                         
 subprocess.run(executable + args, cwd=None, shell=False)                        #Starting of Subprocess
