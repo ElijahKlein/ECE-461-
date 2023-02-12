1eb89199e405aca3ccff34a2c3a2fa63dd2c92a9
Matthew-Nale <71462426+Matthew-Nale@users.noreply.github.com>
2023-02-12 18:41:46 -0500

    Removing old code

--- NetScoreCalculation/MetricCalculation/ramp_up.rs
+++ NetScoreCalculation/MetricCalculation/ramp_up.rs
@@ -45,55 +45,6 @@ pub fn calculate_rampup(readme_size: f64) -> f64{
     }
 }
 
-
-//?Old code to factor in the avg amount of comments per file.
-/*
-//comments_weight will return a weighting for the RampUp time, based on the number of comments
-pub fn calculate_comments(num_comments: f64) -> f64 {
-    //Compares the average number of comments per file to base cases
-    if num_comments <= 2.5 {
-        return 0.0;
-    }
-    else if num_comments <= 5.0 {
-        return 0.1;
-    }
-    else if num_comments <= 10.0 {
-        return 0.2;
-    }
-    else if num_comments <= 15.0 {
-        return 0.3;
-    }
-    else if num_comments <= 20.0 {
-        return 0.4;
-    }
-    else if num_comments <= 25.0 {
-        return 0.5;
-    }
-    else if num_comments <= 30.0 {
-        return 0.6;
-    }
-    else if num_comments <= 35.0 {
-        return 0.7;
-    }
-    else if num_comments <= 40.0 {
-        return 0.8;
-    }
-    else if num_comments <= 45.0 {
-        return 0.9;
-    }
-    else {
-        return 1.0;
-    }
-}
-
-pub fn calculate_rampup(readme_size: f64, num_comments: f64) -> f64{
-    let readme_weighting = calculate_readme(readme_size);
-    let comments_weighting = calculate_comments(num_comments);
-
-    let base_weight = (0.5 * readme_weighting) + (0.5 * comments_weighting);
-    return 1.0;
-}*/
-
 fn main() {
     let args : Vec<String> = env::args().collect();                         //Collects the argv values into a vector called args
     let readme_size : f64 = args[1].parse().unwrap();                       //Converts the string values into a f64 value 
