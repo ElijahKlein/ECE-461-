5034901f418fc4ab750e49ffa489e90d5aec5c1d
Elijah Klein <70978037+ElijahKlein@users.noreply.github.com>
2023-03-20 19:53:42 -0400

    Revert "Main logic repair (#92)"
    This reverts commit 4a4408cfa09f3d8dd679c0b534a1d9b8e4a5a1a9.

--- ErrorTest.py
+++ ErrorTest.py
@@ -1,50 +1,35 @@
-""" Name: Elijah Klein, Eric Chen 
+""" Name: Elijah Klein
     Date of Last Edit: 2/12/2023
-    Purpose: Generate a testing suite that covers the code base
+    
+    Purpose: Used for testing individual functions and function calls, without having to setup Rust-Python integration
+
+    Details: Individual calls can be made below, such as for Licensing.py and repo_clone.py, in order to ensure compatability with all files
 """
-from datetime import datetime
-from Submodules.repo_clone import clone_repo
-from Submodules.npm_handler import npm2git 
-import Submodules.pull_requests as pulls
-import Submodules.readme as readme
-import Submodules.contributors as contrib
+
+import sys
+import Submodules.global_var as gv
 from NetScoreCalculation.MetricCalculation.Licensing import calculateLicenseScore
+from Submodules.repo_clone import clone_repo
 import Submodules.issues as issues
+import Submodules.pull_requests as pulls
+import pytest
+
+
+
 
 url = 'https://github.com/ElijahKlein/461-Test-Case-1'
-url2 = 'https://github.com/ElijahKlein/461-Test-Case-2'
-url3 = 'https://github.com/jaxxk/Sudoku-solver'
 repo = clone_repo(url)                                                          #Clones the repository from the given URL, and a GitPython Repo object is stored in repo
+license_score = calculateLicenseScore(repo)                                     #license_score is determined by the evaluate_readme function in Licensing.py
+print(f'License scoring: {license_score}')
 
-failCount = 0
-numContrib = contrib.getNumContributors(url)
-sumCount = 0
-for i in contrib.getStatsContributors(url):
-	sumCount += i.total
-numFiles = contrib.getNumFiles(url)
-#print(type(issues.getIssueData(url.split("github.com/", 1)[1])))
+numIssues = issues.getIssuesByType(url, 'open')                                 #Example usage of the getIssuesByTypes function, which obtains the number of open issues
+print(f'Number of open issues: {numIssues}')
+numIssues = issues.getIssuesByType(url, 'closed')                                 #Example usage of the getIssuesByTypes function, which obtains the number of open issues
+print(f'Number of closed issues: {numIssues}')
+numDownloads = issues.getUsers(url)                                             #Example usage of the getUsers function, which obtains the number of downloads of the repo
+print(numDownloads)
 
-#functions = [npm2git("https://www.npmjs.com/package/express"), type(issues.getIssueData(url)), issues.getUsers(url), pulls.getMostRecentPull(url, 'closed'), pulls.getMostRecentPull(url, 'open'), pulls.getAllPulls(url, 'closed'), pulls.getAllPulls(url, 'open'), pulls.getAllPulls(url2, 'closed'), pulls.getAllPulls(url2, 'open'), pulls.getCreationDate(url), readme.checkLicensing(repo), readme.checkRMLength(repo), contrib.getNumContributors(url).totalCount, contrib.getNumFiles(url), sumCount, sumCount, contrib.calcContributorsPerFile(numContrib, numFiles)]
-#answers = ["https://github.com/expressjs/express", '.json', 1, (datetime.now() - datetime(2023, 2, 12, 1, 42)).days, None, 2, 0, 0, 1, (datetime.now() - datetime(2023, 2, 12, 9, 14)).days, 1, 707, 2, 5, 9, 9, 0.4]
-
-functions = [npm2git("https://www.npmjs.com/package/express"), issues.getUsers(url), pulls.getMostRecentPull(url, 'closed'), pulls.getMostRecentPull(url, 'open'), pulls.getAllPulls(url, 'closed'), pulls.getAllPulls(url, 'open'), 
-	pulls.getAllPulls(url2, 'closed'), pulls.getAllPulls(url2, 'open'), pulls.getCreationDate(url), readme.checkLicensing(repo), readme.checkRMLength(repo), contrib.getNumContributors(url), contrib.getNumFiles(url), sumCount, sumCount,
-	pulls.getAllPulls(url3, 'closed'), pulls.getAllPulls(url3, 'open'), pulls.getCreationDate(url3), contrib.getNumContributors(url3)]
-
-answers = ["https://github.com/expressjs/express", 1, (datetime.now() - datetime(2023, 2, 12, 1, 42)).days, 0, 2, 0, 0, 1, (datetime.now() - datetime(2023, 2, 12, 9, 14)).days, 1, 707, 2, 5, 9, 9, 0, 0, (datetime.now() - datetime(2020, 5, 23)).days, 4]
-
-#print(f'len: {len(functions)}')
-#print(f'len: {len(answers)}')
-total = len(functions)
-for i in range(0, total):
-	try:
-		assert(functions[i]) == answers[i]
-	except AssertionError as msg:
-		failCount += 1
-		#print(i)
-total += 1
-try:
-	assert(repo) != (None)
-except AssertionError as msg:
-	failCount += 1
-print(f'{total - failCount}/{total} test cases passed. 83% line coverage achieved.') #83% coverage taken from using coverage.py and pytest in conjunction to generate a full coverage report shown in documentation
+recentPull = pulls.getMostRecentPull(url, 'closed')                             #Example usage of the getMostRecentPull function, which obtaines the most recent closed pull request
+print(f'The most recent pull request was: {recentPull} time ago')
+#pullDates = pulls.getAllPullDates(url, 'closed')                               #Example usage of the getAllPullDates function, which obtains a list of all Pull Request dates
+#print(pullDates)
--- NetScoreCalculation/MetricCalculation/ramp_up.rs
+++ NetScoreCalculation/MetricCalculation/ramp_up.rs
@@ -1,25 +1,26 @@
 /*  Name: Matthew Nale
- *  Date of Last Edit: 2/12/2023
+ *  Date of Last Edit: 2/11/2023
  *  
  *  Purpose: Calculate Ramp Up time Sub Metric of a given Github Repository
  *
  *  Details: Using provided data, calculates the Ramp Up time in the range of [0,1], with 1 being the quickest ramp up time and 0 the slowest
 */
+
 use std::env;
 
 //calculate_readme will return a weighting for the ramp up time, based on the size of the readme
 pub fn calculate_rampup(readme_size: f64) -> f64{
     //Compare length of readme_file to base case value
-    if readme_size <= 25.0 {           //25 lines in readme
+    if readme_size <= 25.0 {
         return 0.0;
     }
-    else if readme_size <= 50.0 {      //50 lines in readme
+    else if readme_size <= 50.0 {
         return 0.1;
     }
-    else if readme_size <= 100.0 {     //100 lines in readme
+    else if readme_size <= 100.0 {
         return 0.2;
     }
-    else if readme_size <= 200.0 {     //etc ...
+    else if readme_size <= 200.0 {
         return 0.3;
     }
     else if readme_size <= 300.0 {
@@ -97,6 +98,7 @@ pub fn calculate_rampup(readme_size: f64, num_comments: f64) -> f64{
 fn main() {
     let args : Vec<String> = env::args().collect();                         //Collects the argv values into a vector called args
     let readme_size : f64 = args[1].parse().unwrap();                       //Converts the string values into a f64 value 
+    let num_comments : f64 = args[2].parse().unwrap();
 
     let readme_weight = calculate_rampup(readme_size);
     println!("README size weighting: {readme_weight}");
